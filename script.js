d299110cd6560ff5dc9f1c22c2f4d9ea75afbf0d
Kaif Arslan <kaifarslan1@gmail.com>
2023-04-09 16:33:21 +0530

    There's a problem in finding a good free api

--- script.js
+++ script.js
@@ -1,2 +1,48 @@
-const inp=document.createElement('input')
-document.querySelector('div').innerHTML=inp
+const puzzleBoard= document.querySelector('#puzzle')
+const solveButton= document.querySelector('#solve-button')
+const values = 81;
+const submission= [];
+for(let i=0; i<values;i++){
+  const inputElement=document.createElement("input");
+  inputElement.setAttribute("type",'number');
+  inputElement.setAttribute('min',1);
+  inputElement.setAttribute('max',9);
+  puzzleBoard.appendChild(inputElement);
+}
+
+const joinValues = ()=>{
+  const inputs = document.querySelectorAll('input');
+  inputs.forEach(input=>{
+    if(input.value){
+      submission.push(input.value);
+    }else{
+      submission.push(0);
+    }
+  });
+  console.log(submission);
+}
+
+const solve = ()=>{
+  joinValues();
+  const data = submission;
+  console.log("this is our data", data);
+//---------------------
+const options = {
+  method: 'POST',
+  url: 'https://sudoku-solver3.p.rapidapi.com/sudokusolver/',
+  headers: {
+    'content-type': 'application/json',
+    'X-RapidAPI-Key': '2bdb722bdamsh2258e1305c38fe6p108390jsn2487966d047b',
+    'X-RapidAPI-Host': 'sudoku-solver3.p.rapidapi.com'
+  },
+  data: {input:data}
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+}
+
+solveButton.addEventListener("click",solve);
